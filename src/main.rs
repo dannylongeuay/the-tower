a0b571ce9f1e30a861e33e1cf7529d73eb493178
dannylongeuay <dannylongeuay@yahoo.com>
2022-10-16 16:01:49 -0700

    feat(main): :sparkles: update translation by internal position

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::collections::HashMap;
+
 use bevy::{prelude::*, render::texture::ImageSettings};
 
-const SPRITE_WIDTH: f32 = 8.;
-const SPRITE_HEIGHT: f32 = 8.;
+const SPRITE_WIDTH: i32 = 8;
+const SPRITE_HEIGHT: i32 = 8;
 const SPRITE_SCALE: Vec3 = Vec3::new(1., 1., 1.);
 const PLAYER_SPRITE_INDEX: usize = 4;
+// const FLOOR_SPRITE_INDEX: usize = 17;
 const FLOOR_SPRITE_INDEX: usize = 97;
 
 fn main() {
@@ -21,23 +24,40 @@ fn main() {
         .add_startup_system(setup_system)
         .add_startup_system(load_assets_system)
         .add_startup_system_to_stage(StartupStage::PostStartup, spawn_player_system)
-        .add_startup_system_to_stage(StartupStage::PostStartup, spawn_floor_system)
+        .add_startup_system_to_stage(StartupStage::PostStartup, spawn_tower_system)
         .add_system(input_system)
+        .add_system(update_position_system)
         .run();
 }
 
 #[derive(Component)]
 struct Player;
 
-#[derive(Component)]
+#[derive(Component, PartialEq, Eq, Hash, Clone, Copy)]
 struct Position {
-    x: f32,
-    y: f32,
+    x: i32,
+    y: i32,
 }
 
-#[derive(Component)]
+struct Tile {
+    pos: Position,
+    explored: bool,
+    visible: bool,
+    entities: Vec<Entity>,
+}
+
+struct Map {
+    name: &'static str,
+    tiles: HashMap<Position, Tile>,
+}
+
+struct Level {
+    name: &'static str,
+    maps: HashMap<&'static str, Map>,
+}
+
 struct Tower {
-    tiles: Vec<Position>,
+    levels: HashMap<&'static str, Level>,
 }
 
 struct TextureHandles {
@@ -79,46 +99,77 @@ fn spawn_player_system(mut commands: Commands, texture_handles: Res<TextureHandl
             },
             ..default()
         })
+        .insert(Position { x: 0, y: 0 })
         .insert(Player);
 }
 
-fn spawn_floor_system(mut commands: Commands, texture_handles: Res<TextureHandles>) {
+fn spawn_tower_system(mut commands: Commands, texture_handles: Res<TextureHandles>) {
+    let mut tiles: HashMap<Position, Tile> = HashMap::new();
     for x in -16..16 {
         for y in -16..16 {
-            commands.spawn_bundle(SpriteSheetBundle {
+            let pos = Position { x, y };
+            let mut ec = commands.spawn_bundle(SpriteSheetBundle {
                 sprite: TextureAtlasSprite {
                     index: FLOOR_SPRITE_INDEX,
                     ..default()
                 },
                 texture_atlas: texture_handles.atlas.clone(),
                 transform: Transform {
-                    translation: Vec3::new(x as f32 * SPRITE_WIDTH, y as f32 * SPRITE_HEIGHT, 0.),
                     scale: SPRITE_SCALE,
                     ..default()
                 },
                 ..default()
             });
+            ec.insert(pos);
+            let tile = Tile {
+                pos,
+                explored: false,
+                visible: false,
+                entities: vec![ec.id()],
+            };
+            tiles.insert(pos, tile);
         }
+    }
+    let map = Map {
+        name: "map1",
+        tiles,
+    };
+    let mut maps: HashMap<&str, Map> = HashMap::new();
+    maps.insert(map.name, map);
+    let level = Level {
+        name: "level1",
+        maps,
+    };
+    let mut levels: HashMap<&str, Level> = HashMap::new();
+    levels.insert(level.name, level);
+    let tower = Tower { levels };
+    commands.insert_resource(tower);
+}
+
+fn update_position_system(mut query: Query<(&mut Transform, &Position)>) {
+    for (mut tf, pos) in query.iter_mut() {
+        tf.translation.x = (pos.x * SPRITE_WIDTH) as f32;
+        tf.translation.y = (pos.y * SPRITE_HEIGHT) as f32;
     }
 }
 
 fn input_system(
-    mut query: Query<&mut Transform, With<Player>>,
+    mut query: Query<&mut Position, With<Player>>,
     keyboard_input: Res<Input<KeyCode>>,
 ) {
-    let mut tf = query
+    let mut pos = query
         .get_single_mut()
         .expect("Error: could not find player");
     if keyboard_input.just_pressed(KeyCode::H) {
-        tf.translation.x -= SPRITE_WIDTH;
+        pos.x -= 1;
     }
     if keyboard_input.just_pressed(KeyCode::L) {
-        tf.translation.x += SPRITE_WIDTH;
+        pos.x += 1;
     }
     if keyboard_input.just_pressed(KeyCode::J) {
-        tf.translation.y -= SPRITE_HEIGHT;
+        pos.y -= 1;
     }
     if keyboard_input.just_pressed(KeyCode::K) {
-        tf.translation.y += SPRITE_HEIGHT;
+        pos.y += 1;
     }
 }
